6762bbce76f09d177dec333350ffe1ef4a8a342c
lyonsyonii <liam@garriga.dev>
2024-06-04 20:15:37 +0200

    optimized a bit and improved framerate feedback

--- src/main.rs
+++ src/main.rs
@@ -7,52 +7,54 @@ fn main() {
     let mut buffer = [0; WIDTH * HEIGHT];
 
     let mut window = Window::new(
-        "Test - ESC to exit",
+        "Conwell's Game of Life - By Liam Garriga",
         WIDTH,
         HEIGHT,
         WindowOptions {
             scale: minifb::Scale::X32,
             ..Default::default()
-        }
-    ).unwrap();
-    
-    let mut target_fps = 60;
+        },
+    )
+    .unwrap();
+
+    let target_fps = 60;
     // Limit to max ~60 fps update rate
     window.set_target_fps(target_fps);
-    
+
     let mut tick = 0;
     let mut simulation_speed = 10;
     let mut paused = false;
     while window.is_open() {
         let timing = std::time::Instant::now();
-        
+
         if window.is_key_pressed(Key::Up, minifb::KeyRepeat::Yes) {
             simulation_speed = std::cmp::max(1, simulation_speed - 1);
         } else if window.is_key_pressed(Key::Down, minifb::KeyRepeat::Yes) {
             simulation_speed += 1;
         } else if window.is_key_pressed(Key::Space, minifb::KeyRepeat::No) {
             paused = !paused;
+        } else if window.is_key_pressed(Key::C, minifb::KeyRepeat::No) {
+            buffer.fill(0);
         } else if window.get_mouse_down(minifb::MouseButton::Left) {
             if let Some((y, x)) = window.get_mouse_pos(MouseMode::Discard) {
                 buffer[x as usize * WIDTH + y as usize] = u32::MAX;
             }
         }
-        
+
         if !paused && tick % simulation_speed == 0 {
             simulate(&mut buffer);
-            eprintln!("compute: {:?}", timing.elapsed());
         }
-
+        
         tick += 1;
         
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
-        window
-            .update_with_buffer(&buffer, WIDTH, HEIGHT)
-            .unwrap();
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
 
-        if !paused {
-            eprintln!("framerate: {}", (1f32 / timing.elapsed().as_secs_f32()) / simulation_speed as f32);
-            eprintln!("simulation speed: {}", simulation_speed);
+        if !paused && tick % simulation_speed == 0 {
+            eprintln!(
+                "framerate: {}",
+                (1f32 / (timing.elapsed().as_secs_f32()) / simulation_speed as f32)
+            );
         }
     }
 }
@@ -60,47 +62,28 @@ fn main() {
 fn simulate(cells: &mut [u32; WIDTH * HEIGHT]) {
     const N: usize = WIDTH;
 
-    let clone = *cells;
-    
+    let clone = unsafe { std::mem::transmute_copy::<_, [[u32; WIDTH]; HEIGHT]>(cells) };
+    let get = |i, j| {
+        clone
+            .get(i)
+            .and_then(|row: &[u32; 32]| row.get(j).copied())
+            .unwrap_or_default()
+            >> 31
+    };
     for i in 0..N {
         for j in 0..N {
-            let mut neighbors = 0;
-            if j > 0 {
-                neighbors += clone[i * N + j-1] >> 31;
+            let neighbors: u32 = get(i, j + 1)
+                + get(i, j - 1)
+                + get(i + 1, j)
+                + get(i - 1, j)
+                + get(i + 1, j + 1)
+                + get(i + 1, j - 1)
+                + get(i - 1, j + 1)
+                + get(i - 1, j - 1);
 
-                if i > 0 {
-                    neighbors += clone[(i-1) * N + j-1] >> 31;
-                }
-                if i < N-1 {
-                    neighbors += clone[(i+1) * N + j-1] >> 31;
-                }
-            }
-            if j < N-1 {
-                neighbors += clone[i * N + j+1] >> 31;
-
-                if i > 0 {
-                    neighbors += clone[(i-1) * N + j+1] >> 31;
-                }
-                if i < N-1 {
-                    neighbors += clone[(i+1) * N + j+1] >> 31;
-                }
-            }
-            if i > 0 {
-                neighbors += clone[(i-1) * N + j] >> 31;
-            }
-            if i < N-1 {
-                neighbors += clone[(i+1) * N + j] >> 31;
-            }
             let cell = &mut cells[i * N + j];
-            if *cell != 0 {
-                if neighbors < 2 || neighbors > 3 {
-                    *cell = 0;
-                } else {
-                    *cell = u32::MAX;
-                }
-            } else if neighbors == 3 {
-                *cell = u32::MAX;
-            }
+            *cell *= (neighbors == 2) as u32;
+            *cell |= (neighbors == 3) as u32 * u32::MAX;
         }
     }
 }
